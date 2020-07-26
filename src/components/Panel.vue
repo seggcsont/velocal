44833b192b8634e046c6f2060449457c0a4c2ae8
Laszlo Tarcsanyi <tarcsanyi.laszlo@gmail.com>
2020-07-27 00:33:01 +0200

    Teams editor added

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,5 +1,5 @@
 <template>
-  <fragment>
+  <div>
     <div
       class="relative clearfix m-2 p-2 bg-blue-100 border border-blue-400 rounded-lg text-blue-800"
       :class="[editMode ? 'rounded-b-none mb-0' :'shadow-xl']"
@@ -7,12 +7,12 @@
       <span class="absolute block top-0 right-0 m-2 mt-0 text-3xl cursor-pointer">
         <i v-if="editable" @click="editMode = !editMode" class="fa fa-edit"></i>
       </span>
-      <slot></slot>
+      <slot :editMode="editMode"></slot>
     </div>
     <div v-if="editMode" class="border border-blue-400 border-t-0 rounded-b-lg bg-blue-100 shadow-xl text-blue-800 m-2 mt-0 p-2">
       <slot name="editor">Editor not set</slot>
     </div>
-  </fragment>
+  </div>
 </template>
 
 <script>
@@ -20,7 +20,7 @@ export default {
   name: "Panel",
   data() {
     return {
-      editMode: true,
+      editMode: false,
     };
   },
   props: {
--- src/components/Teams.vue
+++ src/components/Teams.vue
@@ -1,19 +1,81 @@
 <template>
-    <div>
-        <Panel editable>Hello World!</Panel>
-    </div>
+  <div>
+    <Panel editable>
+      <template v-slot:default="slotProps">
+        <table class="table-auto w-full">
+          <thead>
+            <tr>
+              <th class="px-4 py-2">Name</th>
+              <th class="px-4 py-2">Size</th>
+              <th class="px-4 py-2">Members</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-if="teams.length == 0">
+                <td colspan="3" class="text-center font-extrabold text-gray-500">
+                    No teams available
+                </td>
+            </tr>
+            <tr v-for="(team, n) in teams" :key="team.name" :class="[n%2 == 1 ? 'bg-blue-200' : 'bg-blue-300']">
+              <td class="border px-4 py-2">{{team.name}}</td>
+              <td class="border px-4 py-2">{{team.members.length}}</td>
+              <td class="border px-4 py-2">{{team.members.map(member => member.name).join(', ')}}</td>
+              <td class="border px-4 py-2" v-if="slotProps.editMode">
+                <span @click="teams.splice(n,1)">
+                  <i class="fa fa-trash"></i>
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </template>
+      <template v-slot:editor>
+        <label for="team-name">Team name:</label>
+        <input
+          class="ml-2 pl-2 rounded-lg align-text-bottom outline-none focus:shadow-outline"
+          type="text"
+          name="team-name"
+          id="team-name"
+          v-model="newTeamName"
+          @keypress="evt => {if (evt.key === 'Enter') teams.push({name: newTeamName, members:[]}); newTeamName = ''}"
+        />
+      </template>
+    </Panel>
+  </div>
 </template>
 
 <script>
-    import Panel from "./Panel.vue";
-    export default {
-        name: 'Teams',
-        components: {
-            Panel
-        }
-    }
+import Panel from "./Panel.vue";
+export default {
+  name: "Teams",
+  components: {
+    Panel,
+  },
+  data() {
+    return {
+      teams: [
+        {
+          name: "Team-A",
+          members: [
+            {
+              name: "Member 1",
+              country: "US",
+            },
+            {
+              name: "Member 2",
+              country: "HU",
+            },
+          ],
+        },
+        {
+          name: "Team-B",
+          members: [],
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
